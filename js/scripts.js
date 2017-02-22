8107795ed46b21fbbeed551f3cdb9b7f8be02bed
AJ0094 <aheredia@vcd-ch204-21.eastern.ewu.edu>
2017-02-22 13:45:02 -0800

    Randomized Image
    *Random homepage images
    *JS to randomize, not CSS

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
-// YOUR SCRIPTS GO HERE
+//Store 3 Images
+var img0='../img/200.jpg';
+var img1='../img/300.jpg';
+var img2='../img/400.jpg';
+
+//Get Random Number
+var max = 3;
+var numRand = Math.floor( Math.random() * max );
+console.log(numRand);
+
+//Display Randomized Images
+$('img').attr('src', eval('img' + numRand) );
